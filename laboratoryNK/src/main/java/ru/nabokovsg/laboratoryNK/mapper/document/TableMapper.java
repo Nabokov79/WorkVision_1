22934fac9ed529ffd5135511439a0b9417f7222c
NabokovSG <nabokovsg@yandex.ru>
2024-05-13 09:11:04 +0300

    add

--- laboratoryNK/src/main/java/ru/nabokovsg/laboratoryNK/mapper/document/TableMapper.java
+++ laboratoryNK/src/main/java/ru/nabokovsg/laboratoryNK/mapper/document/TableMapper.java
@@ -1,6 +1,7 @@
 package ru.nabokovsg.laboratoryNK.mapper.document;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import ru.nabokovsg.laboratoryNK.model.document.DocumentTable;
 import ru.nabokovsg.laboratoryNK.model.document.report.ProtocolReport;
 import ru.nabokovsg.laboratoryNK.model.template.TableTemplate;
@@ -10,5 +11,11 @@ public interface TableMapper {
 
     DocumentTable mapToDocumentTable(TableTemplate tableTemplate);
 
+    @Mapping(source = "tableTemplate.sequentialNumber", target = "sequentialNumber")
+    @Mapping(source = "tableTemplate.tableName", target = "tableName")
+    @Mapping(source = "tableTemplate.textBeforeTable", target = "textBeforeTable")
+    @Mapping(source = "tableTemplate.textAfterTable", target = "textAfterTable")
+    @Mapping(source = "protocolReport", target = " protocolReport")
+    @Mapping(target = "id", ignore = true)
     DocumentTable mapWithProtocolReport(TableTemplate tableTemplate, ProtocolReport protocolReport);
 }
